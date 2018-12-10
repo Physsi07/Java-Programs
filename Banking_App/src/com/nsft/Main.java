d12bb9b239e12db4267c7ea3b37d8ca26ca4b904
physsi07 <starlinu33@gmail.com>
2018-12-10 08:11:21 -0500

    finished the tutorial of the banking app and all its functions, deposit, withdra, and transaction

---
+++ Banking_App/src/com/nsft/Main.java
@@ -0,0 +1,11 @@
+package com.nsft;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+		BankAccount star = new BankAccount("Starlyn U. V.", "23281839");
+
+		star.showMenu();
+    }
+}
